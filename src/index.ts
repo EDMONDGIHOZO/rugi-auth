567389d803db00fb7fee834891e18ca045d0de53
edmond gihozo <gihozoedmond@gmail.com>
2025-12-02 12:10:21 +0200

    turn into package

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import app from './app';
+
+// Export the Express app
+export { app };
+
+// Export middleware
+export * from './middleware/auth.middleware';
+export * from './middleware/client.middleware';
+export * from './middleware/error.middleware';
+export * from './middleware/rateLimit.middleware';
+export * from './middleware/role.middleware';
+export * from './middleware/superadmin.middleware';
+export * from './middleware/validation.middleware';
+
+// Export services
+export * from './services/app.service';
+export * from './services/auth.service';
+export * from './services/auth-settings.service';
+export * from './services/email.service';
+export * from './services/oauth.service';
+export * from './services/otp.service';
+export * from './services/password.service';
+export * from './services/password-reset.service';
+export * from './services/role.service';
+export * from './services/token.service';
+export * from './services/user.service';
+
+// Export utils
+export * from './utils/logger';
